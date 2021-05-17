44d5555f384637aa935a2bc2e752bdda84bf111e
PutoPug <62641095+PutoPug@users.noreply.github.com>
2021-05-17 12:13:15 +0530

    Think got custom formatting working

--- src/main/java/com/putopug/serverlink/events/ServerLinkEvents.java
+++ src/main/java/com/putopug/serverlink/events/ServerLinkEvents.java
@@ -2,22 +2,27 @@ package com.putopug.serverlink.events;
 
 import com.putopug.serverlink.JDABot;
 import com.putopug.serverlink.ServerLink;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-import static org.bukkit.Bukkit.getServer;
-
 public class ServerLinkEvents implements Listener {
+    public static ServerLink plugin;
     public ServerLinkEvents(ServerLink instance){
         plugin = instance;
     }
-    public static ServerLink plugin;
     @EventHandler
     public static void PlayerChatEvent(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
-        //TODO: Customizable formatting
-        JDABot.smg("**"+event.getPlayer().getDisplayName()+"**: "+event.getMessage());
+        String sendStr = plugin.getConfig().getString("FORMATTING");
+        if(sendStr != null){
+        sendStr = sendStr.replaceAll("\\$PLAYER_NAME",event.getPlayer().getDisplayName()).replaceAll("\\$PLAYER_MESSAGE",event.getMessage());
+        JDABot.smg(sendStr);
+        }else{
+            Bukkit.getLogger().warning("[ServerLink] WARNING: FORMATTING In config.yml is null, falling back to hardcoded fallback message.");
+            JDABot.smg("**"+player.getDisplayName()+"**: "+event.getMessage());
+        }
     }
 }
