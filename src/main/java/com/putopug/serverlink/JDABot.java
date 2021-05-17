d54bdee5d7fcc286e50ec73833a6beebbef68977
PutoPug <62641095+PutoPug@users.noreply.github.com>
2021-05-17 16:24:24 +0530

    Two way chat works!

--- src/main/java/com/putopug/serverlink/JDABot.java
+++ src/main/java/com/putopug/serverlink/JDABot.java
@@ -8,23 +8,28 @@ import net.dv8tion.jda.api.entities.TextChannel;
 import javax.security.auth.login.LoginException;
 import java.util.Objects;
 
-public class JDABot{
+public class JDABot {
     static TextChannel chnl;
+
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        init();
+    }
+
     public static void init()
-            throws LoginException, InterruptedException
-    {
-        JDA jda = JDABuilder.createDefault(ServerLinkEvents.plugin.getConfig().getString("TOKEN"))
-                .build();
+            throws LoginException, InterruptedException {
+        JDA jda = JDABuilder.createDefault(ServerLinkEvents.plugin.getConfig().getString("TOKEN")).addEventListeners(new TwoWayChatListener()).build();
         jda.awaitReady();
-        if(Objects.requireNonNull(ServerLinkEvents.plugin.getConfig().getString("CHANNEL-FIND-MODE")).equalsIgnoreCase("string")){
-            chnl = jda.getTextChannelsByName(Objects.requireNonNull(ServerLinkEvents.plugin.getConfig().getString("CHANNEL")),false).get(0);
-        }else if(Objects.requireNonNull(ServerLinkEvents.plugin.getConfig().getString("CHANNEL-FIND-MODE")).equalsIgnoreCase("id")){
-        chnl = jda.getTextChannelById(Objects.requireNonNull(ServerLinkEvents.plugin.getConfig().getString("CHANNEL")));
-        }else{
+
+        if (Objects.requireNonNull(ServerLinkEvents.plugin.getConfig().getString("CHANNEL-FIND-MODE")).equalsIgnoreCase("string")) {
+            chnl = jda.getTextChannelsByName(Objects.requireNonNull(ServerLinkEvents.plugin.getConfig().getString("CHANNEL")), false).get(0);
+        } else if (Objects.requireNonNull(ServerLinkEvents.plugin.getConfig().getString("CHANNEL-FIND-MODE")).equalsIgnoreCase("id")) {
+            chnl = jda.getTextChannelById(Objects.requireNonNull(ServerLinkEvents.plugin.getConfig().getString("CHANNEL")));
+        } else {
             throw new IllegalArgumentException("FATAL: ServerLink Could Not Parse Channel-Find-Mode , please check the config.yml found in SERVER_ROOT/plugins/ServerLink/config.yml, did you make any spelling mistakes in the Field CHANNEL-FIND-MODE? The only 2 valid options are: string, id");
         }
 
     }
+
     public static void smg(String message) {
         chnl.sendMessage(message).queue();
     }
---
+++ src/main/java/com/putopug/serverlink/TwoWayChatListener.java
@@ -0,0 +1,16 @@
+package com.putopug.serverlink;
+
+import com.putopug.serverlink.events.ServerLinkEvents;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.bukkit.Bukkit;
+import org.jetbrains.annotations.NotNull;
+
+public class TwoWayChatListener extends ListenerAdapter {
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        if(event.getChannel().getName().equals(ServerLinkEvents.plugin.getConfig().getString("CHANNEL"))){
+            Bukkit.broadcastMessage(event.getAuthor().getName()+": "+event.getMessage().getContentDisplay());
+        }
+    }
+}
