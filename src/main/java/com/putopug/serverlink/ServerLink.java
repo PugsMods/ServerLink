85c7ae9fc4d9c880cd0225746c3927a326aefd4a
PutoPug <62641095+PutoPug@users.noreply.github.com>
2021-05-18 09:33:47 +0530

    Startup & Shutdown messages

--- src/main/java/com/putopug/serverlink/ServerLink.java
+++ src/main/java/com/putopug/serverlink/ServerLink.java
@@ -1,6 +1,7 @@
 package com.putopug.serverlink;
 
 import com.putopug.serverlink.events.ServerLinkEvents;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.security.auth.login.LoginException;
@@ -14,10 +15,16 @@ public final class ServerLink extends JavaPlugin {
         try {
             JDABot.init();
         } catch (LoginException | InterruptedException e) {
-            e.printStackTrace();
+            Bukkit.getLogger().severe("[ServerLink] JDA failed to login, did you put the correct token?");
+        }
+        if(this.getConfig().getBoolean("POST-SERVER-STARTUP-MESSAGE")){
+            JDABot.smg("Server started");
         }
     }
     @Override
     public void onDisable() {
+        if(this.getConfig().getBoolean("POST-SERVER-SHUTDOWN-MESSAGE")){
+            JDABot.smg("Shutting down server.");
+        }
     }
 }
