184ad2e8e0daec4202d9f049da180c76db3daa1b
PutoPug <62641095+PutoPug@users.noreply.github.com>
2021-05-18 09:23:52 +0530

    Leave and Join messages

--- src/main/java/com/putopug/serverlink/TwoWayChatListener.java
+++ src/main/java/com/putopug/serverlink/TwoWayChatListener.java
@@ -1,6 +1,7 @@
 package com.putopug.serverlink;
 
 import com.putopug.serverlink.events.ServerLinkEvents;
+import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bukkit.Bukkit;
@@ -9,7 +10,7 @@ import org.jetbrains.annotations.NotNull;
 public class TwoWayChatListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        if(event.getChannel().getName().equals(ServerLinkEvents.plugin.getConfig().getString("CHANNEL"))){
+        if(event.getChannel().getName().equals(ServerLinkEvents.plugin.getConfig().getString("CHANNEL")) && !event.getMessage().getAuthor().isBot()){
             Bukkit.broadcastMessage(event.getAuthor().getName()+": "+event.getMessage().getContentDisplay());
         }
     }
--- src/main/java/com/putopug/serverlink/events/ServerLinkEvents.java
+++ src/main/java/com/putopug/serverlink/events/ServerLinkEvents.java
@@ -7,6 +7,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class ServerLinkEvents implements Listener {
     public static ServerLink plugin;
@@ -16,13 +18,41 @@ public class ServerLinkEvents implements Listener {
     @EventHandler
     public static void PlayerChatEvent(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
-        String sendStr = plugin.getConfig().getString("FORMATTING");
+        String sendStr = plugin.getConfig().getString("CHAT-FORMATTING");
         if(sendStr != null){
         sendStr = sendStr.replaceAll("\\$PLAYER_NAME",event.getPlayer().getDisplayName()).replaceAll("\\$PLAYER_MESSAGE",event.getMessage());
         JDABot.smg(sendStr);
         }else{
-            Bukkit.getLogger().warning("[ServerLink] WARNING: FORMATTING In config.yml is null, falling back to hardcoded fallback message.");
+            Bukkit.getLogger().warning("[ServerLink] WARNING: CHAT-FORMATTING In config.yml is null, falling back to hardcoded fallback message.");
             JDABot.smg("**"+player.getDisplayName()+"**: "+event.getMessage());
+        }
+    }
+    @EventHandler
+    public static void  PlayerJoinEvent(PlayerLoginEvent event){
+        Player player = event.getPlayer();
+        String sendStr = plugin.getConfig().getString("JOIN-FORMATTING");
+        if(plugin.getConfig().getBoolean("POST-JOIN-MESSAGE")){
+        if(sendStr != null){
+            sendStr = sendStr.replaceAll("\\$PLAYER_NAME",event.getPlayer().getDisplayName());
+            JDABot.smg(sendStr);
+        }else{
+            Bukkit.getLogger().warning("[ServerLink] WARNING: JOIN-FORMATTING In config.yml is null, falling back to hardcoded fallback message.");
+            JDABot.smg("**"+player.getDisplayName()+"** joined the game.");
+        }
+        }
+    }
+    @EventHandler
+    public static void  PlayerLeaveEvent(PlayerQuitEvent event){
+        Player player = event.getPlayer();
+        String sendStr = plugin.getConfig().getString("LEAVE-FORMATTING");
+        if(plugin.getConfig().getBoolean("POST-LEAVE-MESSAGE")){
+            if(sendStr != null){
+                sendStr = sendStr.replaceAll("\\$PLAYER_NAME",event.getPlayer().getDisplayName());
+                JDABot.smg(sendStr);
+            }else{
+                Bukkit.getLogger().warning("[ServerLink] WARNING: LEAVE-FORMATTING In config.yml is null, falling back to hardcoded fallback message.");
+                JDABot.smg("**"+player.getDisplayName()+"** left the game.");
+            }
         }
     }
 }
