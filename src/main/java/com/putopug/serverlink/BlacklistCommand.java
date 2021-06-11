11f6a677223b4bcbf14e9b835afabec3be3adc08
PutoPug <62641095+PutoPug@users.noreply.github.com>
2021-06-11 16:17:08 +0530

    Not working stuff

---
+++ src/main/java/com/putopug/serverlink/BlacklistCommand.java
@@ -0,0 +1,18 @@
+package com.putopug.serverlink;
+
+import com.putopug.serverlink.events.ServerLinkEvents;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+
+public class BlacklistCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        return false;
+    }
+}
---
+++ src/main/java/com/putopug/serverlink/DataEngine.java
@@ -0,0 +1,19 @@
+package com.putopug.serverlink;
+
+import com.putopug.serverlink.events.ServerLinkEvents;
+
+import java.io.File;
+import java.io.IOException;
+
+public class DataEngine {
+    public static void loadOrReloadConfig(){
+        File list = new File(ServerLinkEvents.plugin.getDataFolder()+File.separator+"UserPermissionList.json");
+        if(!list.exists()) {
+        try {
+            list.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
+    }
+}
