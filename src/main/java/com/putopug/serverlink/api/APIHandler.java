b3b7ae475aeef5e9e51c7397f55f0fc4879a3f85
PutoPug <62641095+PutoPug@users.noreply.github.com>
2021-06-16 20:04:44 +0530

    Some optimizations

--- src/main/java/com/putopug/serverlink/api/APIHandler.java
+++ src/main/java/com/putopug/serverlink/api/APIHandler.java
@@ -7,22 +7,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
 public class APIHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
+        Map<String, Object> parameters = new HashMap<>();
+        InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), StandardCharsets.UTF_8);
         BufferedReader br = new BufferedReader(isr);
         String query = br.readLine();
         parseQuery(query, parameters);
 
         // send response
-        String response = "";
+        StringBuilder response = new StringBuilder();
         for (String key : parameters.keySet())
-            response += key + " = " + parameters.get(key) + "\n";
+            response.append(key).append(" = ").append(parameters.get(key)).append("\n");
         exchange.sendResponseHeaders(200, response.length());
         OutputStream os = exchange.getResponseBody();
         os.write(response.toString().getBytes());
